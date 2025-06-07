f63a077fc928c9ad6436b282a62c654a3f4943c5
Andrey Snow <148123+andoriyu@users.noreply.github.com>
2025-06-07 12:53:06 -0700

    chore(nix): Add rust overlay to flake (#3)
    * Use rust overlay toolchain
    
    * ci: add flake check workflo

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.naersk.url = "github:nix-community/naersk";
+  inputs.naersk.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, naersk, rust-overlay }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; };
+          naersk-lib = pkgs.callPackage naersk { };
+        in
+        {
+          default = naersk-lib.buildPackage {
+            src = self;
+            cargoLock = ./Cargo.lock;
+          };
+        }
+      );
+
+      devShells = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; }; in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.rust-bin.stable.latest.default
+              pkgs.rust-analyzer
+              pkgs.clippy
+              pkgs.rustfmt
+            ];
+          };
+        }
+      );
+    };
+}
